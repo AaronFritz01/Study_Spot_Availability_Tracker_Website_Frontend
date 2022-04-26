70b967f7a7830b0202323b7a7cbb60ae29a9f0db
Aaron <fritzam@purdue.edu>
2022-04-25 20:22:46 -0400

    feat: added mobile nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,65 @@
 <template>
   <v-app>
+    <v-app-bar app dark color="black" v-if="$vuetify.breakpoint.xs">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-img
+    class="mx-2"
+    src="./assets/Purdue University Logo.svg"
+    max-height="180"
+    max-width="180"
+    min-height="120"
+    min-width="120"
+    contain
+  ></v-img>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary dark app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-img
+    src="./assets/Purdue University Logo.svg"
+    max-height="60"
+    contain
+  ></v-img>
+          <v-list-item-subtitle>
+            Study Spot Availability Tracker
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item
+          v-for="section in sections"
+          :key="section.title"
+          :to="section.path"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+                {{ section.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+    <v-app-bar app dark color="black" v-if="!$vuetify.breakpoint.xs">
+    <v-card color="white"></v-card>
+      <v-img
+    class="mx-2"
+    src="./assets/Purdue University Logo.svg"
+    max-height="180"
+    max-width="180"
+    min-height="120"
+    min-width="120"
+    contain
+  ></v-img>
+  <!-- <v-nav>
+    <v-list></v-list>
+  </v-nav> -->
+  <v-list></v-list>
+    </v-app-bar>
     <v-main>
       <header>
         <div class="gold-stripe"></div>
@@ -12,31 +72,31 @@
         </div>
         <div class="wrapper">
           <nav class="main-nav">
-            <router-link style="text-decoration: none; color: inherit" to="/"
+            <router-link to="/"
               >Home</router-link
             >
             <router-link
-              style="text-decoration: none; color: inherit"
+
               to="/About"
               >About</router-link
             >
             <router-link
-              style="text-decoration: none; color: inherit"
+
               to="/Campus_Map"
               >Campus Map</router-link
             >
             <router-link
-              style="text-decoration: none; color: inherit"
+
               to="/Study_Spaces"
               >Study Spaces</router-link
             >
             <router-link
-              style="text-decoration: none; color: inherit"
+
               to="/Statistics"
               >Statistics</router-link
             >
             <router-link
-              style="text-decoration: none; color: inherit"
+
               to="/Contact"
               >Contact</router-link
             >
@@ -50,10 +110,18 @@
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
 
   data: () => ({
-    //
+    drawer: null,
+    sections: [
+      {title: 'Home', path: '/#'},
+      {title: 'About', path: '/About'},
+      {title: 'Campus Map', path: '/Campus_Map'},
+      {title: 'Study Spaces', path: '/Study_Spaces'},
+      {title: 'Statistics', path: '/Statistics'},
+      {title: 'Contact', path: '/Contact'},
+    ],
   }),
 };
 </script>
